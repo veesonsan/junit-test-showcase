320520c62beacd232f1f388c2ac1f152848b8dab
yushun.qin <yushun.qin@midea.com>
2017-08-26 18:36:35 +0800

    添加注释

--- src/main/java/com/midea/junit/test/domain/Constant.java
+++ src/main/java/com/midea/junit/test/domain/Constant.java
@@ -1,9 +1,28 @@
 package com.midea.junit.test.domain;
 
+/**
+ * 常量累
+ * 
+ * @author <a href="mailto:yushun.qin@midea.com">Yushun.Qin</a>
+ * @createTime 2017年8月26日 下午6:35:44
+ * @since JDK1.7
+ * @version 1.0.0
+ *
+ */
 public class Constant {
 	private Constant() {
 	}
 
+	/**
+	 * 
+	 * 常用key
+	 * 
+	 * @author <a href="mailto:yushun.qin@midea.com">Yushun.Qin</a>
+	 * @createTime 2017年8月26日 下午6:35:56
+	 * @since JDK1.7
+	 * @version 1.0.0
+	 *
+	 */
 	public class Key {
 		public static final String ID = "id";
 		public static final String NAME = "name";
@@ -11,7 +30,5 @@ public class Constant {
 
 		private Key() {
 		}
-
 	}
-
 }
